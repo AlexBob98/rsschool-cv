b0e024f50df90b5de0ac730fa70d5ae373d5018e
AlexBob98 <autoshop98@yandex.ru>
2023-03-17 01:23:36 +0300

    feat: upd info, refactor code

---
+++ json/data.js
@@ -0,0 +1,32 @@
+export const data = [
+    {
+        "img": "./assets/img/projects/stickersp-ru.png",
+        "href": "https://stickersp.ru",
+        "title": "Website Stickersp"
+    },
+    {
+        "img": "./assets/img/projects/alexa.png",
+        "href": "https://rolling-scopes-school.github.io/alexbob98-JSFEPRESCHOOL/portfolio/",
+        "title": "Alexa Rise Portfolio"
+    },
+    {
+        "img": "./assets/img/projects/gallery-img.png",
+        "href": "https://rolling-scopes-school.github.io/alexbob98-JSFEPRESCHOOL/image-galery/",
+        "title": "Image Gallery API"
+    },
+    {
+        "img": "./assets/img/projects/audio-player.png",
+        "href": "https://rolling-scopes-school.github.io/alexbob98-JSFEPRESCHOOL/audio-player/",
+        "title": "Audio Player"
+    },
+    {
+        "img": "./assets/img/projects/tic-tac.png",
+        "href": "https://rolling-scopes-school.github.io/alexbob98-JSFEPRESCHOOL/tic-tac/",
+        "title": "Game Tic Tac Toe"
+    },
+    {
+        "img": "./assets/img/projects/video-player.png",
+        "href": "https://rolling-scopes-school.github.io/alexbob98-JSFEPRESCHOOL/portfolio/",
+        "title": "Video Player"
+    }
+]
---
+++ json/skills.js
@@ -0,0 +1,34 @@
+export const skills = [
+    {
+        "name": "html",
+        "skills": "95%",
+    },
+    {
+        "name": "css",
+        "skills": "95%",
+    },
+    {
+        "name": "javascript",
+        "skills": "85%",
+    },
+    {
+        "name": "TypeScript",
+        "skills": "75%",
+    },
+    {
+        "name": "React",
+        "skills": "65%",
+    },
+    {
+        "name": "git",
+        "skills": "95%",
+    },
+    {
+        "name": "Figma",
+        "skills": "95%",
+    },
+    {
+        "name": "Illustrator",
+        "skills": "95%",
+    },
+]
